43b5c75292b28d0c1d0389baaa5ab993f319e7b4
Andreas Wallner <A.Wallner@innovative-solutions.at>
2020-04-04 15:22:41 +0200

    Add module test build main

--- src/main/scala/innovative-solutions/components.scala
+++ src/main/scala/innovative-solutions/components.scala
@@ -2,11 +2,23 @@ package innovative_solutions
 
 import spinal.core._
 import innovative_solutions.basics.{AxiLite4Pwm, PwmGenerics}
+import innovative_solutions.xilinx._
 
 object AxiLite4PwmTopLevel {
   def main(args: Array[String]) {
     val report = SpinalConfig(defaultConfigForClockDomains =
-      ClockDomainConfig(resetActiveLevel = LOW)
-    ).generateVerilog(XilinxNamer.name(AxiLite4Pwm(PwmGenerics(8, 3))))
+      ClockDomainConfig(resetActiveLevel = LOW),
+      device=Device.XILINX
+    ).generateVerilog(XilinxNamer(AxiLite4Pwm(PwmGenerics(8, 3))))
+  }
+}
+
+object AxiLite4PwmModule {
+  def main(args: Array[String]) {
+    val report = SpinalConfig(defaultConfigForClockDomains =
+      ClockDomainConfig(resetActiveLevel = LOW),
+      device=Device.XILINX
+    ).generateVerilog(AxiLite4Pwm(PwmGenerics(8,3)))
+    VivadoIpify.emitComponentXml(report.toplevel)
   }
 }
