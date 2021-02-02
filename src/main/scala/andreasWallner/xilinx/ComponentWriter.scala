6ccc8caa4e0a06ae02fc362852b68044b70ffae3
Andreas Wallner <A.Wallner@innovative-solutions.at>
2021-02-03 00:48:45 +0100

    Make XilinxRenamer easier to extend

--- src/main/scala/andreasWallner/xilinx/ComponentWriter.scala
+++ src/main/scala/andreasWallner/xilinx/ComponentWriter.scala
@@ -10,14 +10,15 @@ object Helpers {
   implicit class PimpedStringBuilder(val sb: StringBuilder) {
     def ++=(pm: PortMap) = {
       sb ++= s"""
-      |       <spirit:portMap>
-      |         <spirit:logicalPort>
-      |           <spirit:name>${pm.logicalPort}</spirit:name>
-      |         </spirit:logicalPort>
-      |         <spirit:physicalPort>
-      |           <spirit:name>${pm.physicalPort.getName()}</spirit:name>
-      |         </spirit:physicalPort>
-      |       </spirit:portMap>
+                |       <spirit:portMap>
+                |         <spirit:logicalPort>
+                |           <spirit:name>${pm.logicalPort}</spirit:name>
+                |         </spirit:logicalPort>
+                |         <spirit:physicalPort>
+                |           <spirit:name>${pm.physicalPort
+                  .getName()}</spirit:name>
+                |         </spirit:physicalPort>
+                |       </spirit:portMap>
     """.stripMargin
     }
   }
@@ -43,12 +44,12 @@ object VivadoIpify {
   ): StringBuilder = {
     val name = that.definitionName
     builder ++= s"""
-      |<?xml version="1.0" encoding="UTF-8"?>
-      |<spirit:component xmlns:xilinx="http://www.xilinx.com" xmlns:spirit="http://www.spiritconsortium.org/XMLSchema/SPIRIT/1685-2009" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-      | <spirit:vendor>${vendor}</spirit:vendor>
-      | <spirit:library>${library}</spirit:library>
-      | <spirit:name>${name}</spirit:name>
-      | <spirit:version>${version}</spirit:version>
+                   |<?xml version="1.0" encoding="UTF-8"?>
+                   |<spirit:component xmlns:xilinx="http://www.xilinx.com" xmlns:spirit="http://www.spiritconsortium.org/XMLSchema/SPIRIT/1685-2009" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                   | <spirit:vendor>${vendor}</spirit:vendor>
+                   | <spirit:library>${library}</spirit:library>
+                   | <spirit:name>${name}</spirit:name>
+                   | <spirit:version>${version}</spirit:version>
       """.stripMargin
 
     genBusInterfaces(that, builder)
@@ -60,8 +61,8 @@ object VivadoIpify {
     builder ++= "</spirit:description>\n"
 
     builder ++= s"""
-      |</spirit:component>
-      |""".stripMargin
+                   |</spirit:component>
+                   |""".stripMargin
   }
 
   def genBusInterfaces(
@@ -70,16 +71,10 @@ object VivadoIpify {
   ): StringBuilder = {
     builder ++= "  <spirit:busInterfaces>\n"
 
-    ForMasterSlaveInterfaces(that) { (ms, idx) =>
-      val support:Option[BusSupport] = ms match {
-        case axilite: AxiLite4 => Some(AxiLite4Support(axilite))
-        case _                 => println("no:"); println(ms); None
-      }
-      if(support.isDefined) {
-        genBusInterface(support.get, idx)
-        true
-      } else {
-        false
+    forMasterSlaveInterfaces(that) { (ms, idx) =>
+      XilinxSupportFactory.makeSupport(ms) match {
+        case Some(support) => genBusInterface(support, idx); true
+        case None          => false
       }
     }
 
@@ -87,23 +82,23 @@ object VivadoIpify {
       import support.{busType, abstractionType}
       import Helpers._
 
-      val name = support.makeName(idx)
+      val name = support.name(idx)
 
       builder ++= "    <spirit:busInterface>\n"
       builder ++= s"""
-        |      <spirit:name>${name}</spirit:name>
-        |      <spirit:busType spirit:vendor="${busType.vendor}" spirit:library="${busType.library}" spirit:name="${busType.name}" spirit:version="${busType.version}"/>
-        |      <spirit:abstractionType spirit:vendor="${abstractionType.vendor}" spirit:library="${abstractionType.library}" spirit:name="${abstractionType.name}" spirit:version="${abstractionType.version}"/>
-        |      <spirit:slave>
-        |        <spirit:memoryMapRef spirit:memoryMapRef="${name}"/>
-        |      </spirit:slave>
-        |      <spirit:portMaps>""".stripMargin
+                     |      <spirit:name>${name}</spirit:name>
+                     |      <spirit:busType spirit:vendor="${busType.vendor}" spirit:library="${busType.library}" spirit:name="${busType.name}" spirit:version="${busType.version}"/>
+                     |      <spirit:abstractionType spirit:vendor="${abstractionType.vendor}" spirit:library="${abstractionType.library}" spirit:name="${abstractionType.name}" spirit:version="${abstractionType.version}"/>
+                     |      <spirit:slave>
+                     |        <spirit:memoryMapRef spirit:memoryMapRef="${name}"/>
+                     |      </spirit:slave>
+                     |      <spirit:portMaps>""".stripMargin
 
       support.portMaps.foreach((map) => builder ++= map)
 
       builder ++= s"""
-        |     </spirit:portMaps>
-        |""".stripMargin
+                     |     </spirit:portMaps>
+                     |""".stripMargin
       builder ++= "    </spirit:busInterface>\n"
     }
 
