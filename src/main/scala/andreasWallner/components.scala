14249448cf2eac20a0af59af5951a74e09030251
Andreas Wallner <A.Wallner@innovative-solutions.at>
2021-06-03 13:32:44 +0200

    Add main for ISO7816 master

--- src/main/scala/andreasWallner/components.scala
+++ src/main/scala/andreasWallner/components.scala
@@ -7,11 +7,14 @@ import andreasWallner.basics.{AxiLite4Pwm, PwmGenerics}
 import andreasWallner._
 import andreasWallner.xilinx._
 import andreasWallner.io.pwm._
+//import spinal.lib.bus.regif.Document.HtmlGenerator
+import andreasWallner.io.iso7816._
+
 
 object AxiLite4PwmTopLevel {
   def main(args: Array[String]) {
     val report = SpinalConfig(
-      defaultConfigForClockDomains = ClockDomainConfig(resetActiveLevel = HIGH),
+      defaultConfigForClockDomains = ClockDomainConfig(resetActiveLevel = LOW),
       device = Device.XILINX
     ).generateVerilog(XilinxNamer(AxiLite4Pwm(PwmGenerics(8, 3))))
   }
@@ -20,9 +23,9 @@ object AxiLite4PwmTopLevel {
 object AxiLite4PwmModule {
   def main(args: Array[String]) {
     val report = SpinalConfig(
-      defaultConfigForClockDomains = ClockDomainConfig(resetActiveLevel = HIGH),
+      defaultConfigForClockDomains = ClockDomainConfig(resetActiveLevel = LOW),
       device = Device.XILINX
-    ).generateVerilog(AxiLite4Pwm(PwmGenerics(8, 3)))
+    ).generateVerilog(XilinxNamer(AxiLite4Pwm(PwmGenerics(8, 3))))
     VivadoIpify.emitComponentXml(report.toplevel)
   }
 }
@@ -30,7 +33,7 @@ object AxiLite4PwmModule {
 object Apb3PwmModule {
   def main(args: Array[String]) {
     val report = SpinalConfig(
-      defaultConfigForClockDomains = ClockDomainConfig(resetActiveLevel = HIGH),
+      defaultConfigForClockDomains = ClockDomainConfig(resetActiveLevel = LOW),
       device = Device.XILINX
     ).generateVerilog(
       Apb3Pwm(
@@ -39,3 +42,16 @@ object Apb3PwmModule {
     );
   }
 }
+
+object AxiLite4ISO7816Module {
+  def main(args: Array[String]): Unit = {
+    val report = SpinalConfig(
+      defaultConfigForClockDomains = ClockDomainConfig(resetActiveLevel = LOW),
+      defaultClockDomainFrequency = FixedFrequency(100 MHz),
+      device = Device.XILINX
+    ).generateVerilog(
+      XilinxNamer(XilinxInOutWrapper(AxiLite4ISO7816Peripheral()), noClockSpeed=true)
+    );
+  }
+}
+
