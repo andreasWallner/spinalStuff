9588bf0b2788d30f972b7274f02716ef68850a65
Andreas Wallner <A.Wallner@innovative-solutions.at>
2021-07-01 00:46:10 +0200

    Add GPIO module temporarily
    Original module from spinal.lib does currently not allow to remove
    synchronizer from input path (which in our case are already in the
    IO multiplexer)

---
+++ src/test/scala/andreasWallner/spinaltap/SpinalTap.scala
@@ -0,0 +1,24 @@
+package andreasWallner.spinaltap
+
+import spinal.core._
+import spinal.sim._
+import spinal.core.sim._
+import spinal.lib.sim._
+import org.scalatest.funsuite.AnyFunSuite
+import spinal.lib.bus.amba3.apb.sim.Apb3Driver
+
+class SpinalTapTest extends AnyFunSuite {
+  val dut = SimConfig.withWave.compile(new ApbSpinalTap())
+
+  test("try write register") {
+    dut.doSim("try write register") { dut =>
+      SimTimeout(1000)
+      val driver = Apb3Driver(dut.io.bus, dut.clockDomain)
+      dut.clockDomain.forkStimulus(10)
+      driver.write(0x43c00004, 1000)
+
+      dut.clockDomain.waitActiveEdge(10)
+
+    }
+  }
+}
