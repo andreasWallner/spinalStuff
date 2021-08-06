40cfa8ed9c3680027d762f436f8d1e40114001a3
Andreas Wallner <A.Wallner@innovative-solutions.at>
2021-08-07 00:49:05 +0200

    Add BusSlaveFactoryRecorder
    Temporary wrapper class around BusSlaveFactory
    to record register/field metadata

---
+++ src/main/scala/andreasWallner/registers/BusSlaveFactoryRecorder.scala
@@ -0,0 +1,201 @@
+package andreasWallner.registers
+
+import andreasWallner.registers.casemodel._
+import andreasWallner.registers.datamodel.{AccessType, Section, BusComponent}
+import scala.collection.mutable.Map
+import spinal.lib.bus.misc.BusSlaveFactory
+import spinal.core.{Data, Bits, Bool}
+import spinal.lib.{Stream, Flow}
+
+class RegisterRecorder(
+    registers: Map[BigInt, Register],
+    address: BigInt,
+    factory: BusSlaveFactory
+) {
+  private def append(field: Field) = {
+    registers(address) = registers get address match {
+      case None    => Register("", address.toLong, None, List(field))
+      case Some(r) => r.copy(fields = r.fields :+ field)
+    }
+  }
+
+  def read[T <: Data](
+      that: T,
+      bitOffset: Int,
+      name: String,
+      doc: String = null,
+      values: List[Value] = List()
+  ): T = {
+    append(
+      Field(
+        name,
+        that,
+        Section(bitOffset + that.getBitsWidth, bitOffset),
+        AccessType.RO,
+        0,
+        false,
+        doc,
+        values
+      )
+    )
+
+    factory.read(that, address, bitOffset, name)
+  }
+
+  def createReadAndWrite[T <: Data](
+      that: T,
+      bitOffset: Int,
+      name: String,
+      doc: String = null,
+      values: List[Value] = List()
+  ): T = {
+    append(
+      Field(
+        name,
+        that,
+        Section(bitOffset + that.getBitsWidth, bitOffset),
+        AccessType.RW,
+        0,
+        false,
+        doc,
+        values
+      )
+    )
+    factory.createReadAndWrite(that, address, bitOffset, name)
+  }
+
+  def doBitsAccumulationAndClearOnRead(
+      that: Bits,
+      bitOffset: Int,
+      name: String,
+      doc: String = null,
+      values: List[Value] = List()
+  ): Unit = {
+    append(
+      Field(
+        name,
+        that,
+        Section(bitOffset + that.getBitsWidth, bitOffset),
+        AccessType.RC,
+        0,
+        false,
+        doc,
+        values
+      )
+    )
+    factory.doBitsAccumulationAndClearOnRead(that, address, bitOffset)
+  }
+
+  def setOnSet[T <: Data](
+      that: T,
+      bitOffset: Int,
+      name: String,
+      doc: String = null,
+      values: List[Value] = List()
+  ) = {
+    append(
+      Field(
+        name,
+        that,
+        Section(bitOffset + that.getBitsWidth, bitOffset),
+        AccessType.WO,
+        0,
+        false,
+        doc,
+        values
+      )
+    )
+    factory.setOnSet(that, address, bitOffset)
+  }
+
+  def readStreamNonBlocking[T <: Data](
+      that: Stream[T],
+      validBitOffset: Int,
+      payloadBitOffset: Int,
+      name: String,
+      doc: String = null,
+      values: List[Value] = List(),
+      validInverted: Boolean = false
+  ): Unit = {
+    append(
+      Field(
+        name,
+        that.payload,
+        Section(payloadBitOffset + that.payload.getBitsWidth, payloadBitOffset),
+        AccessType.RO,
+        0,
+        false,
+        doc,
+        values
+      )
+    )
+    append(
+      Field(
+        name + "_valid",
+        Bool(),
+        Section(payloadBitOffset),
+        AccessType.RO,
+        0,
+        false,
+        doc,
+        List(
+          Value(
+            if (!validInverted) 0 else 1,
+            "invalid",
+            "Read data value is not valid"
+          ),
+          Value(if (!validInverted) 1 else 0, "valid", "Read data is valid")
+        )
+      )
+    )
+
+    factory.readStreamNonBlocking(
+      that,
+      address,
+      validBitOffset,
+      payloadBitOffset,
+      validInverted
+    )
+  }
+
+  def createAndDriveFlow[T <: Data](
+      dataType: T,
+      bitOffset: Int,
+      name: String,
+      doc: String = null,
+      values: List[Value] = List()
+  ): Flow[T] = {
+    append(
+      Field(
+        name,
+        dataType,
+        Section(dataType.getBitsWidth + bitOffset, bitOffset),
+        AccessType.WO,
+        0,
+        false,
+        doc,
+        values
+      )
+    )
+    factory.createAndDriveFlow(dataType, address, bitOffset)
+  }
+}
+
+class BusSlaveFactoryRecorder(factory: BusSlaveFactory) extends BusComponent {
+  protected val registers = Map[BigInt, Register]()
+
+  def register(
+      address: BigInt,
+      name: String,
+      doc: String = null
+  ): RegisterRecorder = {
+    assert(!registers.contains(address))
+    registers(address) = Register(name, address.toLong, doc, List())
+    new RegisterRecorder(registers, address, factory)
+  }
+
+  override def elements = registers.values
+  override def busComponentName: String = ???
+  override def dataWidth = factory.busDataWidth
+  override def wordAddressInc: Long = factory.wordAddressInc
+}
