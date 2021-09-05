80f96491e98b0ca543e831ec0c1f631b14f024a1
Andreas Wallner <A.Wallner@innovative-solutions.at>
2021-09-06 01:54:25 +0200

    Extend SpinalTap ISO test

--- src/test/scala/andreasWallner/spinaltap/SpinalTap.scala
+++ src/test/scala/andreasWallner/spinaltap/SpinalTap.scala
@@ -10,31 +10,34 @@ import spinal.lib.bus.amba3.apb.sim.Apb3Driver
 class SpinalTapTest extends AnyFunSuite {
   val dut = SimConfig.withWave.compile(new ApbSpinalTap())
 
-  test("try write register") {
-    dut.doSim("try write register") { dut =>
-      SimTimeout(1000)
+  test("configure and run ISO communication") {
+    dut.doSim("configure and run ISO communication") { dut =>
+      SimTimeout(100000)
       val driver = Apb3Driver(dut.io.bus, dut.clockDomain)
+      println(dut.io.port0);
+      //dut.io.port0(0).read #= true // IO
       dut.clockDomain.forkStimulus(10)
 
-      driver.write(0x43c00300, 0x201)
+      val isobase = 0x43c00400;
+      val muxbase = 0x43c00300;
 
-      driver.write(0x43c00104, 0)
-      driver.write(0x43c00108, 0xff)
-      driver.write(0x43c00204, 0)
-      driver.write(0x43c00208, 0xff)
+      driver.write(muxbase, 0x201)
 
-      driver.write(0x43c00104, 1)
-      driver.write(0x43c00104, 0)
-      driver.write(0x43c00104, 1)
-      driver.write(0x43c00104, 0)
+      driver.write(isobase + 0x14, 50)
+      driver.write(isobase + 0x44, 2000)
+      driver.write(isobase + 0x48, 3000)
 
-      driver.write(0x43c00204, 1)
-      driver.write(0x43c00204, 0)
-      driver.write(0x43c00204, 1)
-      driver.write(0x43c00204, 0)
+      driver.write(isobase + 0x18, 2000) // ta
+      driver.write(isobase + 0x1c, 3000) // tb
+      driver.write(isobase + 0x20, 1000) // te
+      driver.write(isobase + 0x24, 1000) // th
+      driver.write(isobase + 0x28, 1000) // vcc_offset
+      driver.write(isobase + 0x2c, 1000) // clk_offset
 
+      driver.write(isobase + 0x10, 0x11)
+
+      while (driver.read(isobase + 0x08) != 0) {}
       dut.clockDomain.waitActiveEdge(10)
-
     }
   }
 }
