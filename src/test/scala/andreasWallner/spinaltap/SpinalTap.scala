9a392f5c82d89ced00fb8456cb942e68e7b57c0a
Andreas Wallner <A.Wallner@innovative-solutions.at>
2021-09-12 22:29:35 +0200

    WIP SpinalTap ISO7816 sim

--- src/test/scala/andreasWallner/spinaltap/SpinalTap.scala
+++ src/test/scala/andreasWallner/spinaltap/SpinalTap.scala
@@ -6,37 +6,80 @@ import spinal.core.sim._
 import spinal.lib.sim._
 import org.scalatest.funsuite.AnyFunSuite
 import spinal.lib.bus.amba3.apb.sim.Apb3Driver
+import andreasWallner.io.iso7816.sim.ISO7816SimClient
+
+import spinal.lib.bus.amba3.apb.Apb3
+import andreasWallner.io.iso7816.{Apb3ISO7816Peripheral, ISO7816}
+import spinal.lib._
+
+object simLog {
+  def apply(s: String) = {
+    println(f"[${Console.BLUE}${simTime()}${Console.RESET}] $s")
+  }
+}
 
 class SpinalTapTest extends AnyFunSuite {
-  val dut = SimConfig.withWave.compile(new ApbSpinalTap())
+  val dut = SimConfig.withWave.compile(new Component() {
+    val spinaltap = new ApbSpinalTap()
+    val io = new Bundle {
+      val bus = slave(Apb3(32, 32))
+      val iso = master(ISO7816())
+    }
+    spinaltap.io.bus <> io.bus
+    io.iso.io <> spinaltap.io.port0(0)
+    io.iso.rst <> spinaltap.io.port0.write(1)
+    io.iso.clk <> spinaltap.io.port0.write(2)
+    io.iso.vcc <> True
+
+    for(i <- 1 to 4)
+      spinaltap.io.port0.read(i) := spinaltap.io.port0.write(i)
+    for(i <- 0 to 4)
+      spinaltap.io.port1.read(i) := False
+  })
 
   test("configure and run ISO communication") {
     dut.doSim("configure and run ISO communication") { dut =>
-      SimTimeout(100000)
+      SimTimeout(500000)
       val driver = Apb3Driver(dut.io.bus, dut.clockDomain)
-      dut.io.port0.read #= 0x7 // IO
-      dut.io.port1.read #= 0x7
+      ISO7816SimClient(dut.io.iso) { client =>
+        val baudrate = 50
+        val clockrate = 10e-6
+        val T = 250 // 500
+
+        dut.clockDomain.waitActiveEdge(10)
+        simLog("waiting for activation")
+        client.waitForActivation()
+        sleep(100 * T)
+        simLog("sending ATR")
+        client.send("3B00", T)
+      }
+      
       dut.clockDomain.forkStimulus(10)
 
       val isobase = 0x43c00400;
       val muxbase = 0x43c00300;
 
-      driver.write(muxbase, 0x304)
+      driver.write(muxbase, 0x403)
 
-      driver.write(isobase + 0x14, 50)
-      driver.write(isobase + 0x44, 2000)
-      driver.write(isobase + 0x48, 3000)
+      driver.write(isobase + 0x14, 25)   // divider
+      driver.write(isobase + 0x4c, 25)
+      driver.write(isobase + 0x44, 0)//40000 * 50) // bwt
+      driver.write(isobase + 0x48, 3000)//40000 * 50) // cwt
 
-      driver.write(isobase + 0x18, 2000) // ta
-      driver.write(isobase + 0x1c, 3000) // tb
+      driver.write(isobase + 0x18, 50 * 100) // ta
+      driver.write(isobase + 0x1c, 50 * 500) // tb
       driver.write(isobase + 0x20, 1000) // te
       driver.write(isobase + 0x24, 1000) // th
       driver.write(isobase + 0x28, 1000) // vcc_offset
       driver.write(isobase + 0x2c, 1000) // clk_offset
 
-      driver.write(isobase + 0x10, 0x11)
+      driver.write(isobase + 0x10, 0x11) // trigger activation & rx
 
       while (driver.read(isobase + 0x08) != 0) {}
+      assert((driver.read(isobase + 0x30) & 0xffff) == 2)
+      assert((driver.read(isobase + 0x3c) & 0xff) == 0x3b)
+      assert((driver.read(isobase + 0x3c) & 0xff) == 0x00)
+      assert((driver.read(isobase + 0x30) & 0xffff) == 0)
       dut.clockDomain.waitActiveEdge(10)
     }
   }
