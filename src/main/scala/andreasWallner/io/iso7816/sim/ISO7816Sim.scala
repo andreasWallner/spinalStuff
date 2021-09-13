35bc378d631deca6367ca62b6ee67d92ef0e19ee
Andreas Wallner <A.Wallner@innovative-solutions.at>
2021-09-14 01:20:16 +0200

    Extend ISO7816 sim client to receive

--- src/main/scala/andreasWallner/io/iso7816/sim/ISO7816Sim.scala
+++ src/main/scala/andreasWallner/io/iso7816/sim/ISO7816Sim.scala
@@ -53,7 +53,9 @@ case class ISO7816SimTx(io: TriState[Bool], baudrate: Double)(
 
   def txByte(data: Int, induceError: Boolean = false): Boolean = {
     val bitsPull = data << 1
-    var parity = (0 to 7).map(i => (data & (1 << i)) != 0).reduce((last, bit) => last ^ bit) ^ induceError
+    var parity = (0 to 7)
+      .map(i => (data & (1 << i)) != 0)
+      .reduce((last, bit) => last ^ bit) ^ induceError
     for (idx <- 0 to 8) {
       io.drive((bitsPull & (1 << idx)) != 0)
       sleep(baudrateLong)
@@ -70,16 +72,19 @@ case class ISO7816SimTx(io: TriState[Bool], baudrate: Double)(
   }
 
   def tx(data: String): Boolean = {
-    data.grouped(2).map(Integer.parseUnsignedInt(_, 16)).foreach(x => {
-      if(txByte(x))
-        return true
-    })
+    data
+      .grouped(2)
+      .map(Integer.parseUnsignedInt(_, 16))
+      .foreach(x => {
+        if (txByte(x))
+          return true
+      })
     return false
   }
 }
 
 case class ISO7816SimClient(iso: ISO7816)(
-  clientFun: (ISO7816SimClient) => Unit
+    clientFun: (ISO7816SimClient) => Unit
 ) {
   iso.io.simulatePullup()
   fork {
@@ -92,11 +97,42 @@ case class ISO7816SimClient(iso: ISO7816)(
 
   def waitForActivation() = {
     iso.io.highz()
-    waitUntil(iso.vcc.toBoolean == true && iso.io.read.toBoolean == true && iso.rst.toBoolean == true)
+    waitUntil(
+      iso.vcc.toBoolean == true && iso.io.read.toBoolean == true && iso.rst.toBoolean == true
+    )
   }
 
   def send(s: String, baudrate: Double) = {
-    val tx = ISO7816SimTx(iso.io, baudrate){(_, _, _) => {}}
+    val tx = ISO7816SimTx(iso.io, baudrate) { (_, _, _) =>
+      {}
+    }
     assert(tx.tx(s) == false, "Unexpected error during TX")
+  }
+
+  def rxByte(baudrate: Double): Int = {
+    var data = 0
+    var parity = false
+    waitUntil(
+      iso.io.writeEnable.toBoolean && !iso.io.write.toBoolean
+    )
+    sleep(baudrate.toLong / 2)
+    for (idx <- 0 to 7) {
+      println(f"${simTime()} $idx ${data}")
+      sleep(baudrate.toLong)
+      data |= (if (iso.io.write.toBoolean) 1 else 0) << idx
+      parity ^= iso.io.write.toBoolean
+    }
+    sleep(baudrate.toLong)
+    sleep(baudrate.toLong)
+    iso.io.drive(true)
+    sleep(((if (!true) 2 else 1) * baudrate.toLong))
+    iso.io.highz()
+
+    data
+  }
+
+  def receive(len: Int, baudrate: Double): List[Int] = {
+    val x = for(idx <- 0 until len) yield rxByte(baudrate)
+    x.toList
   }
 }
