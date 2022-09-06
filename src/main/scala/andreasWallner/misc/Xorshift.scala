287f76a314d6ae63e96126ed50a936d0d2bee716
Andreas Wallner <A.Wallner@innovative-solutions.at>
2022-09-07 01:58:55 +0200

    Extend XorShift for 32/64 bit & add seed input

--- src/main/scala/andreasWallner/misc/Xorshift.scala
+++ src/main/scala/andreasWallner/misc/Xorshift.scala
@@ -3,24 +3,41 @@ package andreasWallner.misc
 import spinal.core._
 import spinal.lib._
 
-case class Xorshift() extends Component {
+case class XorShiftConfig(
+    width: Int,
+    hasRun: Boolean = false,
+    hasSetSeed: Boolean = false
+)
+
+case class Xorshift(config: XorShiftConfig = XorShiftConfig(16))
+    extends Component {
   val io = new Bundle {
-    val run = in Bool()
-    val data = master Stream(Bits(16 bit))
+    val run = config.hasRun generate { in Bool () }
+    val seed = config.hasSetSeed generate { in Bits (config.width bit) }
+    val setSeed = config.hasSetSeed generate { in Bool () }
+    val data = master Stream Bits(config.width bit)
   }
 
-  val state = Reg(Bits(16 bit)) init(1)
+  assert(List(16, 32, 64).contains(config.width))
+  val shifts = config.width match {
+    case 16 => List(7, 9, 8)
+    case 32 => List(13, 17, 5)
+    case 64 => List(13, 7, 17)
+  }
+
+  val state = Reg(Bits(config.width bit)) init 1
   io.data.payload := state
-  io.data.valid := io.run
-  val temp1 = Bits(16 bit)
-  val temp2 = Bits(16 bit)
-  val temp3 = Bits(16 bit)
-  temp1 := state ^ (state |<< 7)
-  temp2 := temp1 ^ (temp1 |>> 9)
-  temp3 := temp2 ^ (temp2 |<< 8)
-  when(!io.run) {
-    state := 1
-  } elsewhen(io.data.valid && io.data.ready) {
+  io.data.valid := (if (config.hasRun) io.run else True) && (if (config.hasSetSeed)
+                                                               !io.setSeed
+                                                             else True)
+  val temp1 = state ^ (state |<< shifts(0))
+  val temp2 = temp1 ^ (temp1 |>> shifts(1))
+  val temp3 = temp2 ^ (temp2 |<< shifts(2))
+  when(io.data.fire) {
     state := temp3
   }
+  if (config.hasSetSeed)
+    when(io.setSeed) {
+      state := io.seed
+    }
 }
