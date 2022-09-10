3ee4fc702780d02dbcbfbf7b1afbc3c8c7f5482c
Andreas Wallner <A.Wallner@innovative-solutions.at>
2022-09-10 16:19:10 +0200

    Add memoization Utility

--- src/main/scala/andreasWallner/package.scala
+++ src/main/scala/andreasWallner/package.scala
@@ -1,5 +1,7 @@
 package andreasWallner
 
+import scala.collection.mutable
+
 object Utils {
   def lcm(a: BigInt, b: BigInt): BigInt = {
     if(a.signum == 0 || b.signum == 0)
@@ -28,4 +30,18 @@ object Utils {
   def gcd(a: BigInt, b: BigInt): BigInt = a.gcd(b)
   def gcd(a: Int, b: Int): Int = gcd(BigInt(a), BigInt(b)).intValue()
   def gcd(a: Long, b: Long): Long = gcd(BigInt(a), BigInt(b)).longValue()
+
+  // Use for memoization, e.g. during simulation
+  // Example:
+  //   lazy val dutFactory: ((Int, Int)) => SimCompiled[MemoryFormatterTester] =
+  //     memoize { case (i, o) =>
+  //       SimConfig.withFstWave.compile(
+  //         Comp(i, o).setDefinitionName(f"Comp_${i}_$o"))}
+  //
+  // Note: current implementation is not thread safe
+  //
+  // Implementation taken from pathikrit (https://stackoverflow.com/a/36960228, CC-BY-SA 3.0)
+  def memoize[I, O](f: I => O): I => O = new mutable.HashMap[I, O]() {
+    override def apply(key: I) = getOrElseUpdate(key, f(key))
+  }
 }
