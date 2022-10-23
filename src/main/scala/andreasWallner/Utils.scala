336342c52f6388c402859a1ddc75193f87e5440e
Andreas Wallner <A.Wallner@innovative-solutions.at>
2022-10-23 02:29:14 +0200

    Comment andreasWallner.Utils & fix filename

--- src/main/scala/andreasWallner/Utils.scala
+++ src/main/scala/andreasWallner/Utils.scala
@@ -3,16 +3,18 @@ package andreasWallner
 import scala.collection.mutable
 
 object Utils {
+  // calculate least common multiple (the smallest number that both a & b divide w/o remainder)
   def lcm(a: BigInt, b: BigInt): BigInt = {
     if(a.signum == 0 || b.signum == 0)
       return BigInt(0)
     a / a.gcd(b) * b
   }
   def lcm(a: Int, b: Int): Int = lcm(BigInt(a), BigInt(b)).intValue()
-  def lcm(a: Long, b: Long): Long = lcm(BigInt(a), BigInt(b)).intValue()
-  def lcm(xs: BigInt*): BigInt = { // TODO check if first elem is negative
+  def lcm(a: Long, b: Long): Long = lcm(BigInt(a), BigInt(b)).longValue()
+  def lcm(xs: BigInt*): BigInt = {
     if (xs.isEmpty)
       return BigInt(1)
+    assert(xs.head >= 0, "First passed BigInt must be positive")
     var lcm = xs(0)
     for (v <- xs.drop(1)) {
       if (v.signum != 0) {
@@ -27,6 +29,7 @@ object Utils {
     lcm
   }
 
+  // calculate greated common divisor (the biggest number that divides both a & b w/o remainder)
   def gcd(a: BigInt, b: BigInt): BigInt = a.gcd(b)
   def gcd(a: Int, b: Int): Int = gcd(BigInt(a), BigInt(b)).intValue()
   def gcd(a: Long, b: Long): Long = gcd(BigInt(a), BigInt(b)).longValue()
