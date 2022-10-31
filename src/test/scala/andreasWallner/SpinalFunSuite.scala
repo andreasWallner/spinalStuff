7bf053d79c8183304e30ea2f2d66133602f2576d
Andreas Wallner <A.Wallner@innovative-solutions.at>
2022-10-31 02:14:53 +0100

    Add method for doSimUntilVoid to SpinalFunSuite

--- src/test/scala/andreasWallner/SpinalFunSuite.scala
+++ src/test/scala/andreasWallner/SpinalFunSuite.scala
@@ -14,16 +14,16 @@ import scala.util.Random
  *
  * <pre class="stHighlight">
  * class SomeTests extends HwFunSuite {
- *   val dut = SimConfig.withFstWave.compile(SomeComponent())
+ * val dut = SimConfig.withFstWave.compile(SomeComponent())
  *
- *   test(dut, "my test description") { dut =>
- *     ...
- *   }
+ * test(dut, "my test description") { dut =>
+ * ...
+ * }
  *
- *   // specifying a seed
- *   test(dut, "my other test", 24) { dut =>
- *     ...
- *   }
+ * // specifying a seed
+ * test(dut, "my other test", 24) { dut =>
+ * ...
+ * }
  * }
  * </pre>
  *
@@ -32,8 +32,11 @@ import scala.util.Random
  */
 class SpinalFunSuite extends AnyFunSuite with BeforeAndAfterAllConfigMap {
   var _globalSeed: Option[Int] = None
-  override def beforeAll(configMap: ConfigMap):Unit = {
-    _globalSeed = configMap.getOptional[java.lang.String]("seed").map(s => java.lang.Integer.parseInt(s))
+
+  override def beforeAll(configMap: ConfigMap): Unit = {
+    _globalSeed = configMap
+      .getOptional[java.lang.String]("seed")
+      .map(s => java.lang.Integer.parseInt(s))
   }
 
   def test[T <: Component](dut: SimCompiled[T], testName: String)(
@@ -41,11 +44,30 @@ class SpinalFunSuite extends AnyFunSuite with BeforeAndAfterAllConfigMap {
   ): Unit = {
     test(dut, testName, Random.nextInt())(body)
   }
+
   def test[T <: Component](dut: SimCompiled[T], testName: String, seed: Int)(
     body: T => Unit
   ): Unit = {
     test(testName) {
-      dut.doSim(testName, seed=_globalSeed.getOrElse(seed)) { dut =>
+      dut.doSim(testName, seed = _globalSeed.getOrElse(seed)) { dut =>
+        body(dut)
+      }
+    }
+  }
+
+  def testUntilVoid[T <: Component](dut: SimCompiled[T], testName: String)(
+    body: T => Unit
+  ): Unit = {
+    test(dut, testName, Random.nextInt())(body)
+  }
+
+  def testUntilVoid[T <: Component](
+                                     dut: SimCompiled[T],
+                                     testName: String,
+                                     seed: Int
+                                   )(body: T => Unit): Unit = {
+    test(testName) {
+      dut.doSimUntilVoid(testName, seed = _globalSeed.getOrElse(seed)) { dut =>
         body(dut)
       }
     }
