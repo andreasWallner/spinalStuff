78a4117f01ad1673f96ae3e80ca6ad25e2baff19
Andreas Wallner <A.Wallner@innovative-solutions.at>
2023-01-09 01:01:56 +0100

    Add small AST dumping utility

--- src/main/scala/andreasWallner/Utils.scala
+++ src/main/scala/andreasWallner/Utils.scala
@@ -3,7 +3,7 @@ package andreasWallner
 import scala.collection.mutable
 
 object Utils {
-  // calculate least common multiple (the smallest number that both a & b divide w/o remainder)
+  /** calculate least common multiple (the smallest number that both a & b divide w/o remainder) */
   def lcm(a: BigInt, b: BigInt): BigInt = {
     if(a.signum == 0 || b.signum == 0)
       return BigInt(0)
@@ -29,22 +29,81 @@ object Utils {
     lcm
   }
 
-  // calculate greated common divisor (the biggest number that divides both a & b w/o remainder)
+  /** calculate greatest common divisor (the biggest number that divides both a & b w/o remainder) */
   def gcd(a: BigInt, b: BigInt): BigInt = a.gcd(b)
   def gcd(a: Int, b: Int): Int = gcd(BigInt(a), BigInt(b)).intValue()
   def gcd(a: Long, b: Long): Long = gcd(BigInt(a), BigInt(b)).longValue()
 
-  // Use for memoization, e.g. during simulation
-  // Example:
-  //   lazy val dutFactory: ((Int, Int)) => SimCompiled[MemoryFormatterTester] =
-  //     memoize { case (i, o) =>
-  //       SimConfig.withFstWave.compile(
-  //         Comp(i, o).setDefinitionName(f"Comp_${i}_$o"))}
-  //
-  // Note: current implementation is not thread safe
-  //
-  // Implementation taken from pathikrit (https://stackoverflow.com/a/36960228, CC-BY-SA 3.0)
+  /**
+   * Use for memoization, e.g. during simulation
+   *
+   * Example:
+   *   lazy val dutFactory: ((Int, Int)) => SimCompiled[MemoryFormatterTester] =
+   *     memoize { case (i, o) =>
+   *       SimConfig.withFstWave.compile(
+   *         Comp(i, o).setDefinitionName(f"Comp_${i}_$o"))}
+   *
+   *  Note: current implementation is not thread safe
+   *  Implementation taken from pathikrit (https://stackoverflow.com/a/36960228, CC-BY-SA 3.0)
+   */
   def memoize[I, O](f: I => O): I => O = new mutable.HashMap[I, O]() {
     override def apply(key: I) = getOrElseUpdate(key, f(key))
+  }
+
+  class DumpAST(indent: String) {
+    import spinal.core._
+    import spinal.core.internals._
+    private lazy val indented = new DumpAST(indent + "  ")
+
+    def printAttributes(d: Data): Unit = {
+      print(if (d.isInOut) d.dirString() + " " else "")
+      print(if (d.isAnalog) "ANA " else "")
+      print(if (d.isReg) s"REG " else "")
+    }
+    def printAttributes(bt: BaseType): Unit = {
+      printAttributes(bt.asInstanceOf[Data])
+      print(if (bt.isReg) s"${bt.clockDomain} " else "")
+    }
+
+    def dump(e: Expression): Unit = {
+      print("E " + indent + e.getClass.toString + " " + e.toString + " (")
+      e match {
+        case bt: BaseType => printAttributes(bt)
+        case d: Data => printAttributes(d)
+        case _ =>
+      }
+      println(")")
+      e.foreachExpression(indented.dump)
+    }
+
+    def dump(s: Statement): Unit = {
+      print("S " + indent + s.getClass.toString + " " + s.toString + " (")
+      s match {
+        case bt: BaseType => printAttributes(bt)
+        case d: Data => printAttributes(d)
+        case _ =>
+      }
+      println(")")
+      s match {
+        case das: DataAssignmentStatement =>
+          new DumpAST(indent + " source ").dump(das.source)
+          new DumpAST(indent + " target ").dump(das.target)
+        case _ => s.foreachExpression(indented.dump)
+      }
+    }
+
+    def dump(c: Component): Unit = {
+      println("C " + indent + c.toString())
+      c.dslBody.foreachStatements(indented.dump)
+      c.children.foreach(indented.dump)
+    }
+  }
+  object DumpAST {
+    import spinal.core.Component
+    import spinal.core.internals.{Statement, Expression}
+    private val d = new DumpAST("")
+    def apply(c: Component): Unit = d.dump(c)
+    def apply(s: Statement): Unit = d.dump(s)
+    def apply(e: Expression): Unit = d.dump(e)
   }
 }
