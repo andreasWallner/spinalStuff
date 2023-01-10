403d0964c17b6cca9137268136f24edf970aa344
Andreas Wallner <A.Wallner@innovative-solutions.at>
2023-01-10 02:47:24 +0100

    Enable HTML register output for ApbSpinalTap

--- src/main/scala/andreasWallner/components.scala
+++ src/main/scala/andreasWallner/components.scala
@@ -1,19 +1,18 @@
 package andreasWallner
 
-import spinal.core._
-import spinal.lib._
-import spinal.lib.io._
 import andreasWallner.basics.{AxiLite4Pwm, PwmGenerics}
-import andreasWallner._
-import andreasWallner.xilinx._
 import andreasWallner.io.pwm._
-import andreasWallner.registers.generator.{CHeader, YAML, CppHeader, KvasirHeader}
-import andreasWallner.spinaltap.MuxConnections
 import andreasWallner.registers.datamodel.BusComponent
+import andreasWallner.registers.generator.{CHeader, CppHeader, HTML, YAML}
+import andreasWallner.spinaltap.MuxConnections
+import andreasWallner.xilinx._
+import spinal.core._
+
+import scala.language.postfixOps
 
 object AxiLite4PwmTopLevel {
   def main(args: Array[String]) {
-    val report = SpinalConfig(
+    SpinalConfig(
       defaultConfigForClockDomains = ClockDomainConfig(resetActiveLevel = HIGH),
       device = Device.XILINX
     ).generateVerilog(XilinxNamer(AxiLite4Pwm(PwmGenerics(8, 3))))
@@ -32,14 +31,14 @@ object AxiLite4PwmModule {
 
 object Apb3PwmModule {
   def main(args: Array[String]) {
-    val report = SpinalConfig(
+    SpinalConfig(
       defaultConfigForClockDomains = ClockDomainConfig(resetActiveLevel = HIGH),
       device = Device.XILINX
     ).generateVerilog(
       Apb3Pwm(
         Pwm.PeripheralParameters(Pwm.CoreParameters(channelCnt = 5))
       )
-    );
+    )
   }
 }
 
@@ -88,17 +87,18 @@ object ApbSpinalTap {
       device = Device.XILINX,
       targetDirectory = "generated"
     ).generateVerilog(
-      XilinxNamer/*(XilinxInOutWrapper*/(new andreasWallner.spinaltap.ApbSpinalTap())/*)*/
-    );
-
+      XilinxNamer(/*XilinxInOutWrapper*/(new andreasWallner.spinaltap.ApbSpinalTap()))
+    )
     new MuxConnections.CppHeader(report.toplevel.muxConnections, Some("spinaltap::iomux")).write(f"${GlobalData.get.phaseContext.config.targetDirectory}/iomux_connection.hpp")
     for (e <- report.toplevel.elements) {
       e match {
         case (b: BusComponent, offset: Long) =>
+          println(f"${b.busComponentName} @ $offset%x")
           new CHeader(b).write()
           new CppHeader(b, namespace=Some(s"spinaltap::${b.busComponentName.toLowerCase}::registers")).write()
           //new KvasirHeader(b, namespace=Some(s"spinaltap::register::${b.busComponentName.toLowerCase}")).write()
           new YAML(b).write()
+          new HTML(b).write()
         case other => println("Not generating for " + other)
       }
     }
