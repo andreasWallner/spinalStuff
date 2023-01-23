a90e223508135d63a51503bfebb43dedb9cce892
Andreas Wallner <A.Wallner@innovative-solutions.at>
2023-01-23 01:30:10 +0100

    Also dump tags with DumpAST

--- src/main/scala/andreasWallner/Utils.scala
+++ src/main/scala/andreasWallner/Utils.scala
@@ -3,9 +3,10 @@ package andreasWallner
 import scala.collection.mutable
 
 object Utils {
+
   /** calculate least common multiple (the smallest number that both a & b divide w/o remainder) */
   def lcm(a: BigInt, b: BigInt): BigInt = {
-    if(a.signum == 0 || b.signum == 0)
+    if (a.signum == 0 || b.signum == 0)
       return BigInt(0)
     a / a.gcd(b) * b
   }
@@ -22,7 +23,7 @@ object Utils {
         if (gcd == 1) {
           lcm = lcm * v
         } else if (v != gcd) {
-          lcm = lcm * (v/gcd)
+          lcm = lcm * (v / gcd)
         }
       }
     }
@@ -35,17 +36,17 @@ object Utils {
   def gcd(a: Long, b: Long): Long = gcd(BigInt(a), BigInt(b)).longValue()
 
   /**
-   * Use for memoization, e.g. during simulation
-   *
-   * Example:
-   *   lazy val dutFactory: ((Int, Int)) => SimCompiled[MemoryFormatterTester] =
-   *     memoize { case (i, o) =>
-   *       SimConfig.withFstWave.compile(
-   *         Comp(i, o).setDefinitionName(f"Comp_${i}_$o"))}
-   *
-   *  Note: current implementation is not thread safe
-   *  Implementation taken from pathikrit (https://stackoverflow.com/a/36960228, CC-BY-SA 3.0)
-   */
+    * Use for memoization, e.g. during simulation
+    *
+    * Example:
+    *   lazy val dutFactory: ((Int, Int)) => SimCompiled[MemoryFormatterTester] =
+    *     memoize { case (i, o) =>
+    *       SimConfig.withFstWave.compile(
+    *         Comp(i, o).setDefinitionName(f"Comp_${i}_$o"))}
+    *
+    *  Note: current implementation is not thread safe
+    *  Implementation taken from pathikrit (https://stackoverflow.com/a/36960228, CC-BY-SA 3.0)
+    */
   def memoize[I, O](f: I => O): I => O = new mutable.HashMap[I, O]() {
     override def apply(key: I) = getOrElseUpdate(key, f(key))
   }
@@ -55,45 +56,64 @@ object Utils {
     import spinal.core.internals._
     private lazy val indented = new DumpAST(indent + "  ")
 
-    def printAttributes(d: Data): Unit = {
+    def printFlags(d: Data): Unit = {
       print(if (d.isInOut) d.dirString() + " " else "")
       print(if (d.isAnalog) "ANA " else "")
       print(if (d.isReg) s"REG " else "")
     }
     def printAttributes(bt: BaseType): Unit = {
-      printAttributes(bt.asInstanceOf[Data])
+      printFlags(bt.asInstanceOf[Data])
       print(if (bt.isReg) s"${bt.clockDomain} " else "")
     }
 
-    def dump(e: Expression): Unit = {
-      print("E " + indent + e.getClass.toString + " " + e.toString + " (")
-      e match {
-        case bt: BaseType => printAttributes(bt)
-        case d: Data => printAttributes(d)
+    val tMark = s"${Console.GREEN}T${Console.RESET}"
+    def printTags(str: SpinalTagReady): Unit = {
+      for (tag <- str.getTags()) {
+        println(s"$tMark $indent ${tag.getClass.getName} ${tag.toString}")
+      }
+    }
+    def printTags(bn: BaseNode): Unit = {
+      bn match {
+        case str: SpinalTagReady => printTags(str.asInstanceOf[SpinalTagReady])
         case _ =>
       }
+    }
+
+    val eMark = s"${Console.YELLOW}E${Console.RESET}"
+    def dump(e: Expression): Unit = {
+      print(s"$eMark $indent ${e.getClass.toString} ${e.toString} (")
+      e match {
+        case bt: BaseType => printAttributes(bt)
+        case d: Data      => printFlags(d)
+        case _            =>
+      }
       println(")")
+      indented.printTags(e)
       e.foreachExpression(indented.dump)
     }
 
+    val sMark = s"${Console.RED}S${Console.RESET}"
     def dump(s: Statement): Unit = {
-      print("S " + indent + s.getClass.toString + " " + s.toString + " (")
+      print(s"$sMark $indent ${s.getClass.toString} ${s.toString} (")
       s match {
         case bt: BaseType => printAttributes(bt)
-        case d: Data => printAttributes(d)
-        case _ =>
+        case d: Data      => printFlags(d)
+        case _            =>
       }
       println(")")
       s match {
         case das: DataAssignmentStatement =>
-          new DumpAST(indent + " source ").dump(das.source)
-          new DumpAST(indent + " target ").dump(das.target)
+          new DumpAST(indent + "   source ").dump(das.source)
+          new DumpAST(indent + "   target ").dump(das.target)
         case _ => s.foreachExpression(indented.dump)
       }
+      indented.printTags(s)
     }
 
+    val cMark = s"${Console.BLUE}C${Console.RESET}"
     def dump(c: Component): Unit = {
-      println("C " + indent + c.toString())
+      println(s"$cMark $indent ${c.toString()}")
+      indented.printTags(c)
       c.dslBody.foreachStatements(indented.dump)
       c.children.foreach(indented.dump)
     }
