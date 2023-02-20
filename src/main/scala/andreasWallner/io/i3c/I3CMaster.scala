2d016e8139a54462a6b5e28652fe50e4ab82a451
Andreas Wallner <A.Wallner@innovative-solutions.at>
2023-02-20 02:55:51 +0100

    Minimal work on I3C

--- src/main/scala/andreasWallner/io/i3c/I3CMaster.scala
+++ src/main/scala/andreasWallner/io/i3c/I3CMaster.scala
@@ -4,11 +4,15 @@ import spinal.core._
 import spinal.lib._
 import spinal.lib.io.TriState
 
-case class I3C() extends Bundle with IMasterSlave {
-  val scl, sda = TriState(Bool)
+case class I3C(slaveOnly: Boolean = false) extends Bundle with IMasterSlave {
+  val scl, sda = TriState(Bool())
+  val pullup = if(!slaveOnly) Some(Bool()) else None
+  val keeper = if(!slaveOnly) Some(Bool()) else None
 
   def asMaster(): Unit = {
     master(scl, sda)
+    pullup.map(b => out(b))
+    keeper.map(b => out(b))
   }
 }
 
