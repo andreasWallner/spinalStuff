eb9fdd87267574724c6b31c2eab389f5c7348c0e
Andreas Wallner <A.Wallner@innovative-solutions.at>
2023-10-09 00:27:57 +0200

    Use Lattice IOs in iCEBreakerFlow

--- src/main/scala/andreasWallner/demos/icebreaker/iCEBreakerFlow.scala
+++ src/main/scala/andreasWallner/demos/icebreaker/iCEBreakerFlow.scala
@@ -1,10 +1,12 @@
 package andreasWallner.demos.icebreaker
 
+import andreasWallner.demos.icebreaker.Dot.report
 import andreasWallner.eda.YosysFlow
+import andreasWallner.eda.YosysFlow.dotDiagram
 import andreasWallner.rtlutils._
 import spinal.core._
 import spinal.lib.eda.bench.Rtl
-import spinal.lib.io.InOutWrapper
+import andreasWallner.xilinx.InOutWrapper
 
 import scala.language.postfixOps
 import scala.tools.nsc.io.File
@@ -12,14 +14,20 @@ import scala.tools.nsc.io.File
 class iCEBreakerFlow[T <: Component](
     c: => T,
     workspace: String = "demo",
-    forceDownload: Boolean = false
+    forceDownload: Boolean = false,
+    allowUnconstrained: Boolean = false,
+    generateDot: Boolean = false,
+    verbose: Boolean = false
 ) extends App {
   val report = SpinalConfig(
     targetDirectory = workspace,
     defaultConfigForClockDomains = ClockDomainConfig(resetKind = BOOT),
     defaultClockDomainFrequency = FixedFrequency(12 MHz),
     device = Device.LATTICE
-  ).generateVerilog { InOutWrapper(c) }
+  ).generateVerilog { InOutWrapper(c, InOutWrapper.LatticeIce40SB_IO) }
+  if (generateDot) {
+    dotDiagram(workspace, Rtl(report))
+  }
   val synth = YosysFlow(
     workspace,
     Rtl(report),
@@ -28,7 +36,8 @@ class iCEBreakerFlow[T <: Component](
     "sg48",
     frequencyTarget = Some(report.toplevel.getFastestClock()),
     pcfFile = Some(File(".").toCanonical + "/extras/iCEBreaker.pcf"),
-
+    allowUnconstrained = allowUnconstrained,
+    verbose = verbose
   )
   val fmaxDec = synth.fmax().decompose
   println(f"${fmaxDec._1}%.1f ${fmaxDec._2}")
