2fdabcda8b01b165293941e9dcb4ffb1fe192236
Andreas Wallner <A.Wallner@innovative-solutions.at>
2024-04-11 23:37:02 +0200

    Update wiring of S=10 master

--- src/main/scala/andreasWallner/demos/Iso7816S10Master.scala
+++ src/main/scala/andreasWallner/demos/Iso7816S10Master.scala
@@ -20,27 +20,27 @@ case class Iso7816S10Master() extends Component {
 
   val module = S10MasterPeripheral[Apb3](4, Apb3(apbConfig), Apb3SlaveFactory(_))
   module.io.bus <> bridge.io.apb
-  io.ledGreen := module.io.busy
-  io.leds(0) := io.pmod(0).read
-  io.leds(3 downto 1) := 0
+  io.leds(0) := io.pmod(4).read
+  io.leds(1) := io.pmod(5).read
+  io.leds(2) := io.pmod(6).read
+  io.leds(3) := module.io.busy
+  io.ledGreen := io.pmod(7).read
 
-  io.pmod(0) <> module.io.iso.data
-  io.pmod(1).write := module.io.iso.clk
-  io.pmod(2).write := module.io.iso.rst
-  io.pmod(3).write := module.io.vcc
-  io.pmod(1).writeEnable := True
-  io.pmod(2).writeEnable := True
-  io.pmod(3).writeEnable := True
-
-  io.pmod(4).write := module.by1024.core.io.rsp.valid.pull()
-  io.pmod(4).writeEnable := True
-  io.pmod(5).write := module.by1024.core.io.rsp.payload.pull()
+  io.pmod(4) <> module.io.iso.data
+  io.pmod(5).write := module.io.iso.clk
+  io.pmod(6).write := module.io.iso.rst
+  io.pmod(7).write := module.io.vcc
   io.pmod(5).writeEnable := True
-  io.pmod(6).write := module.rspFifo.io.push.valid.pull()
   io.pmod(6).writeEnable := True
-  for(i <- 7 until 8) {
+  io.pmod(7).writeEnable := True
+
+  io.pmod(0).write := io.pmod(4).read
+  io.pmod(1).write := io.pmod(5).read
+  io.pmod(2).write := io.pmod(6).read
+  io.pmod(3).write := io.pmod(7).read
+  for(i <- 0 until 4) {
     io.pmod(i).write := False
-    io.pmod(i).writeEnable := False
+    io.pmod(i).writeEnable := True
   }
 }
 
@@ -66,6 +66,6 @@ object Iso7816S10Master extends App {
   println(synth.getArea())
 
   import scala.sys.process._
-  if(args.contains("prog") || true)
+  if(args.contains("prog"))
     println(s"iceprog ${synth.bitstreamFile.get}"!!)
 }
